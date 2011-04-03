00ab8da9f49db84177e737e425f07fcb2749acfc
Nathan Smith <nathan@quirky.com>
2011-04-03 14:15:44 -0400

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flowup::Application.config.secret_token = 'cbd78ac57f9295cd274b29b44cb079a761a03ecfe248f615f64613077ed6c9cd7ad63c71a2162497d829db040f30e9e79532d70b4c2ea3de6db29236aab04609'
